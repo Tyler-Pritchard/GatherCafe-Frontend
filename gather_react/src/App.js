b2c4972486518c36f1157f13d41a52043484c895
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-01-08 17:52:47 -0800

    connected to backend

--- gather_react/src/App.js
+++ gather_react/src/App.js
@@ -4,20 +4,34 @@ import "./App.css";
 import Navbar from "./common/Navbar";
 import Footer from "./common/Footer";
 import Menu from "./Components/Menu";
-import Cart from "./Components/Cart";
-import Item from "./Components/Item";
+import ItemsList from "./Components/ItemsList";
 import Login from "./Components/Login";
 import UserProfile from "./Components/UserProfile";
 
 class App extends Component {
+  //******************STATE********************* */
+  state = {
+    menu: [],
+    items: []
+  };
+
+  async componentDidMount() {
+    const itemsJSON = await fetch(`http://localhost:5000/items`);
+    const items = await itemsJSON.json();
+    console.log("hey");
+    this.setState({
+      // menus: menu,
+      ...items
+    });
+  }
+
   render() {
     return (
       <Router>
         <div>
           <Navbar />
           <Menu />
-          <Item />
-          <Cart />
+          <ItemsList items={this.state.items} />
           <Route exact path="/login" component={() => <Login />} />
           <Route exact path="/userprofile" component={() => <UserProfile />} />
           <Footer />
--- gather_react/src/Components/Menu.jsx
+++ gather_react/src/Components/Menu.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { Button, Image, List } from "semantic-ui-react";
 
-const Menu = () => (
-  <List divided verticalAlign="middle">
-    <List.Item>
-      <List.Content floated="right">
-        <Button>Add</Button>
-      </List.Content>
-      <Image avatar src="/images/avatar/small/lena.png" />
-      <List.Content>Item</List.Content>
-      <List.Content>$Price</List.Content>
-      <List.Content>Description</List.Content>
-      {/* addon checkbox */}
-    </List.Item>
-  </List>
-);
+class Menu extends React.Component {
+  // async componentDidMount() {
+  //   let menusJSON = await fetch("http://localhost:5000/menus");
+  //   let menus = await menusJSON.json();
+  //   console.log(menus);
+  // }
+  render() {
+    return (
+      <List divided verticalAlign="middle">
+        <List.Item>
+          <List.Content floated="right">
+            <Button>Add</Button>
+          </List.Content>
+          <Image avatar src="/images/avatar/small/lena.png" />
+          <List.Content>Item</List.Content>
+          <List.Content>$Price</List.Content>
+          <List.Content>Description</List.Content>
+          {/* addon checkbox */}
+        </List.Item>
+      </List>
+    );
+  }
+}
 
 export default Menu;
