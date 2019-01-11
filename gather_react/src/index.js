b8c952d2c94eebdb3e80b24a181c8eb1cfb76e51
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-01-10 22:42:38 -0800

    changing menu & item routes in models

--- gather_react/src/index.js
+++ gather_react/src/index.js
@@ -5,9 +5,11 @@ import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
 import { Provider } from "react-redux";
 import { fetchItems } from "./redux/actions/items";
+import { fetchMenus } from "./redux/actions/menus";
 import store from "./redux/store";
 const newStore = store();
 newStore.dispatch(fetchItems());
+newStore.dispatch(fetchMenus());
 
 ReactDOM.render(
   <Provider store={newStore}>
