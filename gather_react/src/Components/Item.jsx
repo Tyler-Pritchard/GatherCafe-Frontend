7766b4224e7b95831166a26f96c21c575bc095f4
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-01-13 20:29:29 -0800

    added images to menus with AWS EC2

--- gather_react/src/Components/Item.jsx
+++ gather_react/src/Components/Item.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Card, Image, Button } from "semantic-ui-react";
+import LazyImage from "../common/LazyImage";
 
 const Item = props => {
-  console.log(props);
+  console.log(props, "THESE ARE THE PROPS YOU ARE LOOKING FOR");
   let { name, description, price, image_url } = props.item;
   return !props.item ? null : (
     <div>
-      <Card fluid link raised>
-        <Image src={image_url} />
+      <Card fluid link raised className="itemCard">
+        <LazyImage src={image_url} size="" rounded />
         <Card.Content>
           <Card.Header>{name}</Card.Header>
           <Card.Description>{description}</Card.Description>
--- gather_react/src/Components/Menu.jsx
+++ gather_react/src/Components/Menu.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { Card, Image, Button } from "semantic-ui-react";
+import { Card, Button } from "semantic-ui-react";
 
 const Menu = props => {
   console.log(props);
-  let { name, description, image_url } = props.menu;
+  let { name } = props.menu;
   return !props.menu ? null : (
     <div>
       <Card fluid link raised>
-        <Image src={image_url} />
         <Card.Content>
           <Card.Header>{name}</Card.Header>
-          <Card.Description>{description}</Card.Description>
         </Card.Content>
         <Button>{name}</Button>
       </Card>
