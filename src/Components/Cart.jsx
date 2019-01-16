ff63190721e57a6a8c5fa83cfbae87f3eac9c761
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-01-15 21:57:35 -0800

    added in stripe payment

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -2,12 +2,22 @@ import React, { Component } from "react";
 import { Button, Image, List } from "semantic-ui-react";
 import { connect } from "react-redux";
 import { removeItemInCart } from "../redux/actions/items";
+import StripeCheckout from "react-stripe-checkout";
+
 class Cart extends Component {
+  state = {
+    totalInCents: null
+  };
+
   calculateTotal = () => {
     let totalInCents = this.props.itemsInCart.reduce((acc, agg) => {
       return acc + +agg.price;
     }, 0);
     return totalInCents / 100;
+  };
+
+  onToken = token => {
+    console.log(token);
   };
 
   render() {
@@ -33,7 +43,12 @@ class Cart extends Component {
             <List.Content>$Tax</List.Content>
             <List.Content>$Total</List.Content>
             <List.Content floated="right">
-              <Button>Checkout</Button>
+              {/* <Button>Checkout</Button> */}
+              <StripeCheckout
+                token={this.onToken}
+                stripeKey={"pk_test_SAPK0hauZtBaTRT3jC6tD33F"}
+                amount={10000}
+              />
             </List.Content>
           </List.Item>
         </div>
