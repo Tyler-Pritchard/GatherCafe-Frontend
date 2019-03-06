6eed8648758796770e4258a7d78ca5749ae1a0cc
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-03-05 21:37:52 -0800

    added menu test boilerplate

--- src/Components/__tests__/Menu.test.js
+++ src/Components/__tests__/Menu.test.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+import { mount } from "enzyme";
+import Menu from "Components/Menu";
 
-import Menu from 'Components/Menu'
+let wrapped;
 
-it('shows a list of menu items', () => {
+beforeEach(() => {
+  wrapped = mount(<Menu />);
+});
 
-})
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it("shows a list of menu items", () => {
+  const wrapped = mount(<Menu />);
+
+  //TODO: expect menu items, add/delete buttons
+});
