03cd9b86cb8064506766737890b245ef91323ef3
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-03-05 21:49:08 -0800

    added cart test boilerplate

--- src/Components/__tests__/Cart.test.js
+++ src/Components/__tests__/Cart.test.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
+import { mount } from "enzyme";
+import Cart from "Components/Cart";
 
-import Cart from 'Components/Cart'
+let wrapped;
 
-it('has a list of items and a button', () => {
+beforeEach(() => {
+  wrapped = mount(() => {
+    mount(<Cart />);
+  });
+});
 
-})
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it("has a list of items and checkout button", () => {
+  const wrapped = mount(<Cart />);
+
+  //TODO expect(wrapped.find("itemsList").length).toEqual(1);
+  //expect(wrapped.find("button").length).toEqual(1);
+});
