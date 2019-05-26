be23dfca4ae19b05fe94d9bb9834eb4072b3c9b2
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-05-26 12:14:22 -0700

    add initial state to auth reducer

--- src/Components/GoogleAuth.jsx
+++ src/Components/GoogleAuth.jsx
@@ -22,7 +22,7 @@ class GoogleAuth extends React.Component {
 
   onAuthChange = isSignedIn => {
     if (isSignedIn) {
-      this.props.signIn(this.auth.currentUser.get().getId());
+      this.props.signIn();
     } else {
       this.props.signOut();
     }
--- src/redux/reducers/authReducer.jsx
+++ src/redux/reducers/authReducer.jsx
@@ -1,17 +1,5 @@
-import { SIGN_IN, SIGN_OUT } from "../actions/types";
-
 const INITIAL_STATE = {
-  isSignedIn: null,
-  userId: null
+  isSignedIn: null
 };
 
-export default (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case SIGN_IN:
-      return { ...state, isSignedIn: true, userId: action.payload };
-    case SIGN_OUT:
-      return { ...state, isSignedIn: false, userId: null };
-    default:
-      return state;
-  }
-};
+export default (state = INITIAL_STATE, action) => {};
