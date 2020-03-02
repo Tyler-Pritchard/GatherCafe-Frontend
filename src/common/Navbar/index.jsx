0bc137ca61082781aa2d9a2d18454a61bb7d8b21
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-03-02 14:08:49 -0800

    styling menu bar

--- src/common/Navbar/index.jsx
+++ src/common/Navbar/index.jsx
@@ -4,6 +4,7 @@ import GoogleAuth from "../../Components/GoogleAuth";
 import { ReactComponent as CartIcon} from '../../Images/Icons/SVG/cart.svg';
 import Img from 'react-image';
 import Spinner from '../Spinner';
+import logo from "../../assets/logos/coffee-logo-design-creative-idea-logo-elements-2.png";
 
 
 
@@ -14,28 +15,18 @@ import Spinner from '../Spinner';
     showFixedMenu = () => this.setState({ fixed: true });
 
     render() {
-      const { children } = this.props;
       const { fixed } = this.state;
 
       return (
         <ul
           className="navbar"
           fixed={fixed ? "top" : null}
-          // inverted={!fixed}
-          // pointing={!fixed}
-          // secondary={!fixed}
         >
         <div className="navbar__container">
-
           <div className="navbar__logo-box">
-          <Link to="/">
-            <Img
-              className="navbar__logo"
-              src={"../../assets/logos/coffee-logo-design-creative-idea-logo-elements-2.png"}
-              loader={<Spinner />}
-              unloader={"Galvanize Home"}
-                />
-          </Link>
+            <img src={logo} alt="Logo" className="navbar__logo" loader={<Spinner />} 
+            unloader={"Great Cafe Home"}  
+            />
           </div>
             
           <div className="navbar__menu">
@@ -62,7 +53,6 @@ import Spinner from '../Spinner';
             >
               <Link to="/userprofile">Sign Up</Link>
             </div>
-
           </section>
           
           <div className="navbar__Oauth">
@@ -82,139 +72,3 @@ import Spinner from '../Spinner';
   }
 }
 export default Navbar;
-
-// import {
-//   Button,
-//   Container,
-//   Image,
-//   Icon,
-//   Menu,
-//   Responsive,
-//   Segment,
-//   Sidebar,
-//   Visibility
-// } from "semantic-ui-react";
-
-// const getWidth = () => {
-//   const isSSR = typeof window === "undefined";
-
-//   return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth;
-// };
-
-// class DesktopContainer extends Component {
-//   state = {};
-
-//   hideFixedMenu = () => this.setState({ fixed: false });
-//   showFixedMenu = () => this.setState({ fixed: true });
-
-//   render() {
-//     const { children } = this.props;
-//     const { fixed } = this.state;
-
-//     return (
-//       <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
-//         <Visibility
-//           once={false}
-//           onBottomPassed={this.showFixedMenu}
-//           onBottomPassedReverse={this.hideFixedMenu}
-//         >
-
-// DesktopContainer.propTypes = {
-//   children: PropTypes.node
-// };
-
-// class MobileContainer extends Component {
-//   state = {};
-
-//   handleSidebarHide = () => this.setState({ sidebarOpened: false });
-
-//   handleToggle = () => this.setState({ sidebarOpened: true });
-
-//   render() {
-//     const { children } = this.props;
-//     const { sidebarOpened } = this.state;
-
-//     return (
-//       <Responsive
-//         as={Sidebar.Pushable}
-//         getWidth={getWidth}
-//         maxWidth={Responsive.onlyMobile.maxWidth}
-//       >
-//         <Sidebar
-//           className="navbar__side-bar"
-//           as={Menu}
-//           animation="push"
-//           inverted
-//           onHide={this.handleSidebarHide}
-//           vertical
-//           visible={sidebarOpened}
-//         >
-//           <div className="navbar__logo-box">
-//                 <Link to="/">
-//                   <img
-//                     className="navbar__logo"
-//                     src={"../../assets/logos/coffee-logo-design-creative-idea-logo-elements-2.png"}
-//                     alt="Galvanize Home"
-//                      />
-//                 </Link>
-//                 </div>
-
-//           <Menu.Item>
-//             <Link to="/menu">Menu List</Link>
-//           </Menu.Item>
-
-//           <Menu.Item>Catering</Menu.Item>
-
-//           <Menu.Item>
-//             <Link to="/cart">
-//               <CartIcon />
-//             </Link>
-//           </Menu.Item>
-//         </Sidebar>
-
-//         <Sidebar.Pusher dimmed={sidebarOpened}>
-//           <Segment
-//             inverted
-//             textAlign="center"
-//             style={{ minHeight: 10, padding: "1em 0em" }}
-//             vertical
-//           >
-//             <Container class="navbar">
-//               <Menu inverted pointing secondary size="large">
-//                 <Menu.Item onClick={this.handleToggle}>
-//                   <Icon name="sidebar" />
-//                 </Menu.Item>
-//                 <Menu.Item position="right">
-//                   <Button>
-//                     <Link to="/login">Log in</Link>
-//                   </Button>
-//                   <Button inverted style={{ marginLeft: "0.5em" }}>
-//                     <Link to="/userprofile">Sign Up</Link>
-//                   </Button>
-//                 </Menu.Item>
-//                 {/* <Menu.Item position="right"> <GoogleAuth /> </Menu.Item> */}
-//               </Menu>
-//             </Container>
-//           </Segment>
-//           {children}
-//         </Sidebar.Pusher>
-//       </Responsive>
-//     );
-//   }
-// }
-// MobileContainer.propTypes = {
-//   children: PropTypes.node
-// };
-// const ResponsiveContainer = ({ children }) => (
-//   <div>
-//     <DesktopContainer>{children}</DesktopContainer>
-//     <MobileContainer>{children}</MobileContainer>
-//   </div>
-// );
-
-// ResponsiveContainer.propTypes = {
-//   children: PropTypes.node
-// };
-
-// const Navbar = () => <ResponsiveContainer />;
-
